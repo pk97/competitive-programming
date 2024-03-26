f34108b30a7560f41dfcabf22ece02d8891f4940
Pulkit Kogta <pulkitkogta@Pulkits-MacBook-Pro-2.local>
2024-03-26 18:19:52 +0530

    Prefix sum approch to find larget subarray of sum k

---
+++ src/daily/challenge/arrays/LongestSubarrayForSizeK.java
@@ -0,0 +1,55 @@
+package daily.challenge.arrays;
+
+import java.util.HashMap;
+
+public class LongestSubarrayForSizeK {
+
+    public static int longestSubarrayWithSumK(int []a, long k) {
+        // Write your code here
+        int st = 0,  end=0, ans=0;
+        long sum=0;
+        while(  end < a.length) {
+            while(sum < k && end < a.length) {
+                end++;
+                if (end < a.length) sum = sum +a[end];
+            }
+            while(sum == k && end < a.length) {
+                ans = Math.max(ans, end-st +1);
+                end++;
+                if (end < a.length) sum = sum +a[end];
+
+            }
+            if(sum > k) {
+                sum -= a[st];
+                st++;
+            }
+        }
+        return ans;
+    }
+
+    public static int getLongestSubarray(int []nums, int k) {
+        // Write your code here.
+        HashMap<Integer, Integer> map = new HashMap();
+        map.put(0,0);
+        int ps = 0;
+        int ans=0;
+        for(int i=0;i< nums.length;i++) {
+            ps += nums[i];
+            if (!map.containsKey(ps)) {
+                map.put(ps, i+1);
+            }
+            if (map.conatinsKey(ps -k)) {
+                ans = Math.max(ans, i - map.get(ps -k));
+            }
+        }
+        return ans;
+
+
+    }
+
+    public static void main(String[] args) {
+        int k = longestSubarrayWithSumK(new int[] {1, 2, 1, 3}, 2);
+
+        System.out.println(k);
+    }
+}
--- src/daily/challenge/arrays/notes.md
+++ src/daily/challenge/arrays/notes.md
@@ -5,3 +5,12 @@
     There is only one repeated number in nums, return this repeated number.
     
     You must solve the problem without modifying the array nums and uses only constant extra space.
+
+**Prefix sum approach**
+Q) Find largest subarray of sum k. elements can be negative.
+
+![img.png](img.png)
+
+- Idea is to iterate & keep adding sum. if prefixSum == k then you have your answer.
+  Also keep storing the prefix sum in a hashmap with index i. Now find x = prefixSum - k. 
+  If x is present in hasMap, it means there exist a a subArray of sum k ending at that particular index i.
