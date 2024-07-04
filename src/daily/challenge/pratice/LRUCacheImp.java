cba293f2d6e8a276adb0e5c1340edcd64dfd1648
PUKOGTA <thoughtworks.kogta@extaccount.com>
2024-07-04 09:06:12 +0530

    LRUCache & Map

--- src/daily/challenge/pratice/LRUCacheImp.java
+++ src/daily/challenge/pratice/LRUCacheImp.java
@@ -1,2 +1,53 @@
-package daily.challenge.pratice;public class LRUCache {
+package daily.challenge.pratice;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class LRUCacheImp {
+    static class LRUCache<K, V> extends LinkedHashMap<K, V> {
+        private final int capacitry;
+
+        LRUCache(int capacity) {
+//            super(capacity, 0.75f, true);
+            this.capacitry = capacity;
+        }
+
+        @Override
+        protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
+            return size() > capacitry;
+        }
+    }
+
+//    public static class LRUCache<K, V> extends LinkedHashMap<K, V> {
+//        private final int capacity;
+//
+//        public LRUCache(int capacity) {
+//            super(capacity, 0.75f, true);
+//            this.capacity = capacity;
+//        }
+//
+//        @Override
+//        protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+//            return size() > capacity;
+//        }
+//    }
+
+        public static void main(String[] args) {
+        LRUCache cache = new LRUCache<String, Integer>(3);
+        cache.put("1",1);
+        cache.put("2",121);
+        cache.put("3",1);
+
+        System.out.println(cache);
+        System.out.println(cache.get("2"));
+        cache.put("4",4);
+        System.out.println(cache.toString());
+
+
+
+
+        }
 }
--- src/daily/challenge/pratice/MapInDecreasingOrder.java
+++ src/daily/challenge/pratice/MapInDecreasingOrder.java
@@ -1,2 +1,27 @@
-package daily.challenge.pratice;public class MapInDecreasingOrder {
+package daily.challenge.pratice;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class MapInDecreasingOrder {
+
+    public static void ans(String[]  s) {
+        Map<String, Integer> map  =new HashMap<>();
+
+        for (String x: s) {
+            map.put(x, map.getOrDefault(x, 0)+1);
+        }
+        Queue<Map.Entry<String, Integer>> q = new PriorityQueue<>( (a,b) -> b.getValue() - a.getValue());
+
+        for ( Map.Entry<String, Integer> entry : map.entrySet()) {
+            q.offer(entry);
+        }
+        q.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        ans(new String[] {"A", "B", "A", "B", "C", "A"});
+    }
 }
