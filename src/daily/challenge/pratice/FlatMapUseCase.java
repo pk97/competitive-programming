10c7db9b540e337b1e53d13e933de24888e7b9fc
PUKOGTA <thoughtworks.kogta@extaccount.com>
2024-07-04 09:05:43 +0530

    Flatmap usecase

--- src/daily/challenge/pratice/FlatMapUseCase.java
+++ src/daily/challenge/pratice/FlatMapUseCase.java
@@ -1,2 +1,14 @@
-package daily.challenge.pratice;public class FlatMapUseCase {
+package daily.challenge.pratice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlatMapUseCase {
+    public static void main(String[] args) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        List<Integer> collect = list.stream()
+                    .flatMap(x -> x.stream())
+                    .toList();
+    }
 }
