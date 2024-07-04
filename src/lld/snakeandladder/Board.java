246149627bc1ee82cdb0a5495db799bd1ffbc46b
PUKOGTA <thoughtworks.kogta@extaccount.com>
2024-07-04 09:03:53 +0530

    LLD snake and ladder

--- src/lld/snakeandladder/Board.java
+++ src/lld/snakeandladder/Board.java
@@ -1,2 +1,15 @@
-package lld.snakeandladder;public class Board {
+package lld.snakeandladder;
+
+public class Board {
+    private Box[][] positions;
+
+     Board() {
+         positions = new Box[10][10];
+         for (int i = 0; i < 10; i++) {
+             for (int j = 0; j < 10; j++) {
+                 positions[i][j] = new Box(i*10+j+1);
+             }
+
+         }
+    }
 }
--- src/lld/snakeandladder/Box.java
+++ src/lld/snakeandladder/Box.java
@@ -1,2 +1,16 @@
-package lld.snakeandladder;public class Box {
+package lld.snakeandladder;
+
+public class Box {
+    private int number;
+    private Character character;
+
+    Box(int number, Character character) {
+        this.number = number;
+        this.character = character;
+    }
+
+    Box(int number) {
+        this.number = number;
+        character = null;
+    }
 }
